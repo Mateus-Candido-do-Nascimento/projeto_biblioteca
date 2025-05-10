07a48dd9b33cfea563aec46893c3ba4396190ab5
Mateus-prime <matturushihara@gmail.com>
2025-05-10 19:26:12 -0300

    reset
    apagar tudo

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'segredo-super-secreto'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///biblioteca.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
